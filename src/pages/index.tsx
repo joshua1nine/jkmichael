cd9df25a7a907e4b65b5879d5ffc0e2dc568c589
Joshua Michael <joshua.kale.michael@gmail.com>
2024-02-28 15:40:24 -0600

    Update metadata, add og:image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,30 @@
-import Header from '../components/Header';
-import Page from '../components/Page';
-import { Work } from '../components/Work';
-import { Hero } from '../components/Hero';
-import { About } from '../components/About';
-import { Contact } from '../components/Contact';
+import Header from "../components/Header";
+import Page from "../components/Page";
+import { Work } from "../components/Work";
+import { Hero } from "../components/Hero";
+import { About } from "../components/About";
+import { Contact } from "../components/Contact";
+import { Metadata } from "next";
+import Head from "next/head";
 
 const Home = () => {
-	return (
-		<Page>
-			<Header />
-			<div className='container mx-auto max-w-7xl p-3 lg:p-5 min-h-screen relative'>
-				<main>
-					<Hero />
-					<About />
-					<Work />
-					<Contact />
-				</main>
-			</div>
-		</Page>
-	);
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Hey!" key="title" />
+      </Head>
+      <Page>
+        <Header />
+        <div className="container mx-auto max-w-7xl p-3 lg:p-5 min-h-screen relative">
+          <main>
+            <Hero />
+            <About />
+            <Work />
+            <Contact />
+          </main>
+        </div>
+      </Page>
+    </>
+  );
 };
 export default Home;
