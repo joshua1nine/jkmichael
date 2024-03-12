507f7e9fcd2858b91973b3c76bced7e57c3bd924
Joshua Michael <joshua.kale.michael@gmail.com>
2024-03-12 14:16:21 -0500

    Update hero copy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,32 +23,30 @@ export const Hero = () => {
   return (
     <section
       ref={ref}
-      className="relative mt-8 mb-36 md:mt-16 md:mb-56 lg:mt-32 lg:mb-64"
+      className="relative mt-8 mb-36 md:mt-16 md:mb-56 lg:mt-32 lg:mb-64 flex flex-col items-center"
     >
-      <motion.h2
-        variants={popInVariants}
-        initial="hidden"
-        animate={mainControls}
-        transition={{ delay: 0.1 }}
-        className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl flex flex-col gap-0 uppercase font-bold mb-2"
-      >
-        <span>Michael</span>
-        <span className="text-orange">Web Solutions</span>
-      </motion.h2>
+      {/* <motion.h2 */}
+      {/*   variants={popInVariants} */}
+      {/*   initial="hidden" */}
+      {/*   animate={mainControls} */}
+      {/*   transition={{ delay: 0.1 }} */}
+      {/*   className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl flex flex-col gap-0 uppercase font-bold mb-2" */}
+      {/* > */}
+      {/*   <span>Michael</span> */}
+      {/*   <span className="text-orange">Web Solutions</span> */}
+      {/* </motion.h2> */}
       <motion.p
         variants={popInVariants}
         initial="hidden"
         animate={mainControls}
         transition={{ delay: 0.2 }}
-        className="py-2 mb-8 text-xl max-w-4xl md:leading-10 md:text-3xl"
+        className="py-2 mb-8 text-4xl font-black max-w-4xl md:leading-[120%] md:text-6xl text-center"
       >
         Elevate your business with{" "}
         <span className="gradient-text animate-gradient">bespoke</span> websites
         designed to{" "}
         <span className="gradient-text animate-gradient">engage</span> and{" "}
-        <span className="gradient-text animate-gradient">convert</span>, craft
-        pixel-perfect experiences that{" "}
-        <span className="gradient-text animate-gradient">drive success</span>.
+        <span className="gradient-text animate-gradient">convert</span>.
       </motion.p>
       <motion.button
         onClick={() => router.push("/#work")}
@@ -56,7 +54,7 @@ export const Hero = () => {
         initial="hidden"
         animate={mainControls}
         transition={{ delay: 0.2 }}
-        className="text-white text-lg font-medium px-4 py-3 rounded-md overflow-hidden relative transition-transform hover:scale-105 active:scale-95"
+        className="text-white text-md md:text-lg w-52 font-medium px-4 py-3 rounded-md overflow-hidden relative transition-transform hover:scale-105 active:scale-95"
       >
         <span className="relative z-10">Check out my work</span>
         <motion.div
@@ -68,7 +66,8 @@ export const Hero = () => {
             duration: 4,
             ease: "linear",
           }}
-          className="bg-[linear-gradient(to_right,#ec6b2d,#ee7b44,#f08c5b)] absolute z-0 inset-0 w-[400%]"
+          // className="bg-[linear-gradient(to_right,#ec6b2d,#F29C73)] absolute z-0 inset-0 w-[400%]"
+          className="bg-[linear-gradient(to_right,#ec6b2d,#f08c5b)] absolute z-0 inset-0 w-[400%]"
         ></motion.div>
       </motion.button>
     </section>
